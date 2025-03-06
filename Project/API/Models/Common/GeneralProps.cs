6fdd31f9b15929fc454733b38223884b2d4812ba
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-03-06 21:40:26 +0700

    change name common class

---
+++ Project/API/Models/Common/GeneralProps.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models.Common
+{
+    public abstract class GeneralProps
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
