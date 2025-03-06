634f8082779d73c24c71de005c2528b6a1112fd7
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-03-06 22:42:04 +0700

    create employeetype entity

---
+++ Project/API/Models/Entities/EmployeeType.cs
@@ -0,0 +1,18 @@
+﻿using API.Models.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.Entities
+{
+    [Table("EmployeeType")]
+    public class EmployeeType : GeneralProps
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string? Name { get; set; }
+        [StringLength(500)]
+        public string? Description { get; set; }
+    }
+}
