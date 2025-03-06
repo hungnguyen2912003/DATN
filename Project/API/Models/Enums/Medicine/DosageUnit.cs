62eae05cc34b8f4ccce32919fec5f55a8845eb1c
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-03-06 23:11:59 +0700

    create new enums

---
+++ Project/API/Models/Enums/Medicine/DosageUnit.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Models.Enums.Medicine
+{
+    public enum DosageUnit
+    {
+        Mg = 1,
+        Viên = 2,
+        Ml = 3,
+        G = 4,
+        Muỗng = 5
+    }
+}
---
+++ Project/API/Models/Enums/Medicine/UnitType.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models.Enums.Medicine
+{
+    public enum UnitType
+    {
+        Viên = 1,
+        Hộp = 2,
+        Chai = 3,
+        Ống = 4
+    }
+}
