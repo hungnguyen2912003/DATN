6420d8beab4411728ba5bbd592e0a2eaf21bbea9
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-03-06 23:12:56 +0700

    create medicine entity

---
+++ Project/API/Models/Entities/Medicine.cs
@@ -0,0 +1,49 @@
+﻿using API.Models.Common;
+using API.Models.Enums.Medicine;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.Entities
+{
+    [Table("Medicine")]
+    public class Medicine : GeneralProps
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string? Description { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string? Image { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int InStock { get; set; }
+        [Required]
+        public int UnitQuantity { get; set; }
+        [Required]
+        [StringLength(50)]
+        public UnitType UnitType { get; set; }
+        [Required]
+        public decimal DosageQuantity { get; set; }
+        [Required]
+        [StringLength(50)]
+        public DosageUnit DosageUnit { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string? Manufacturer { get; set; }
+        [Required]
+        public DateTime ManufacturedDate { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string? ActiveIngredient { get; set; }
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+        [Required]
+        public bool Status { get; set; }
+    }
+}
