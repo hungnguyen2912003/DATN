f0cfbd5b58e0211575bc121d5732b58455d34fa7
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-03-06 22:35:07 +0700

    Create Employee entity

---
+++ Project/API/Models/Entities/Employee.cs
@@ -0,0 +1,46 @@
+﻿using API.Models.Common;
+using API.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.Entities
+{
+    [Table("Employee")]
+    public class Employee : GeneralProps
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? FullName { get; set; }
+        [Required]
+        public GenderType Gender { get; set; }
+        [Required]
+        [StringLength(12, MinimumLength = 9)]
+        public string? IdentityCard { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        [Phone]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string? Address { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string? Degree { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string? ProfessionalQualification { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string? Image { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public EmployeeStatus Status { get; set; }
+    }
+}
