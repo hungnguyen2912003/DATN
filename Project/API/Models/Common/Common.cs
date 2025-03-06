d5a9a7fa7b2adaa7db1d7b8832b439b29d4462d2
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-03-06 21:30:37 +0700

    initial common class

---
+++ Project/API/Models/Common/Common.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models.Common
+{
+    public class Common
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
