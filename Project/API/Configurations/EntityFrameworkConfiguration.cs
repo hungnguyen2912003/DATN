0ab9e5b030dce19076620d0734e0837c164f504a
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-08 18:30:31 +0700

    organize program.cs fil

---
+++ Project/API/Configurations/EntityFrameworkConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using API.Models.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Configurations
+{
+    public static class EntityFrameworkConfiguration
+    {
+        public static IServiceCollection InjectDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDBContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
+    }
+}
---
+++ Project/API/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace API.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static IServiceCollection AddSwaggerExplorer(this IServiceCollection services)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+
+        public static WebApplication ConfigureSwaggerExplorer(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            return app;
+        }
+    }
+}
