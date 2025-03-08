b6c26ade593402a70cd57c01eebce39ab418e31c
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-08 17:19:55 +0700

    create dbcontext and add emp, dep in dbcontext

---
+++ Project/API/Models/Common/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models.Common
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Employee> employees { get; set; }
+        DbSet<Department> departments { get; set; }
+    }
+}
