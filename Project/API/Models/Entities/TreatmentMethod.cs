29738505fd0fe4730afb0f911e64169502055ba8
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-08 12:36:17 +0700

    create treatment method entity

---
+++ Project/API/Models/Entities/TreatmentMethod.cs
@@ -0,0 +1,31 @@
+﻿using API.Models.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.Entities
+{
+    [Table("TreatmentMethod")]
+    public class TreatmentMethod : GeneralProps
+    {
+        // ID
+        [Key]
+        public Guid Id { get; set; }
+        // Mã phương pháp
+        [Required]
+        [StringLength(20)]
+        public string? TreatmentMethodCode { get; set; }
+        // Tên phương pháp
+        [Required]
+        [StringLength(50)]
+        public string? TreatmentMethodName { get; set; }
+        // Mô tả
+        [StringLength(500)]
+        public string? Description { get; set; }
+        // Chi phí
+        [Required]
+        public decimal Cost { get; set; }
+        [Required]
+        // Trạng thái
+        public bool Status { get; set; }
+    }
+}
