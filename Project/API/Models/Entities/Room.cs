e2f232ad52535854bc5a46cea73e4fd5cf439dd9
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-08 12:26:33 +0700

    create room entity

---
+++ Project/API/Models/Entities/Room.cs
@@ -0,0 +1,28 @@
+﻿using API.Models.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.Entities
+{
+    [Table("Room")]
+    public class Room : GeneralProps
+    {
+        // ID
+        [Key]
+        public Guid Id { get; set; }
+        // Mã phòng
+        [Required]
+        [StringLength(20)]
+        public string? RoomCode { get; set; }
+        [Required]
+        [StringLength(50)]
+        // Tên phòng
+        public string? RoomName { get; set; }
+        [StringLength(500)]
+        // Mô tả
+        public string? Description { get; set; }
+        [Required]
+        // Trạng thái phòng
+        public bool Status { get; set; }
+    }
+}
