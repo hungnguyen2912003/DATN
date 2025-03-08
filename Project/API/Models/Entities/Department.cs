e9fdd25d106ba4d0e6c7a2b7773203c69d5e75b1
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-08 12:00:11 +0700

    create department entity

---
+++ Project/API/Models/Entities/Department.cs
@@ -0,0 +1,25 @@
+﻿using API.Models.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.Entities
+{
+    [Table("Department")]
+    public class Department : GeneralProps
+    {
+        [Key]
+        // Mã phòng ban
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        // Tên phòng ban
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(500)]
+        // Mô tả
+        public string? Description { get; set; }
+        [Required]
+        // Trạng thái phòng ban
+        public bool Status { get; set; }
+    }
+}
