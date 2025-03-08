27fa13499acf4b8a4b72d326434b321f02783fae
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-08 12:45:44 +0700

    create medicine type entity

---
+++ Project/API/Models/Entities/MedicineType.cs
@@ -0,0 +1,32 @@
+﻿using API.Models.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.Entities
+{
+    [Table("MedicineType")]
+    public class MedicineType : GeneralProps
+    {
+        [Key]
+        // ID
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        // Mã loại thuốc
+        public string? MedicineTypeCode { get; set; }
+        [Required]
+        [StringLength(50)]
+        // Tên loại thuốc
+        public string? Name { get; set; }
+        [StringLength(500)]
+        // Mô tả
+        public string? Description { get; set; }
+        [Required]
+        [StringLength(500)]
+        // Hình ảnh
+        public string? Image { get; set; }
+        [Required]
+        // Trạng thái
+        public bool Status { get; set; }
+    }
+}
