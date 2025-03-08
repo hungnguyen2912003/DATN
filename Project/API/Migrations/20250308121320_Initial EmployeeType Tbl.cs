19ed00cfa1358b6d424a0f1b63e860e0ba63098e
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-08 19:15:20 +0700

    add migration emp - emptype

---
+++ Project/API/Migrations/20250308121320_Initial EmployeeType Tbl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialEmployeeTypeTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "EmployeeTypeId",
+                table: "Employee",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeeTypeCode = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeType", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_EmployeeTypeId",
+                table: "Employee",
+                column: "EmployeeTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_EmployeeType_EmployeeTypeId",
+                table: "Employee",
+                column: "EmployeeTypeId",
+                principalTable: "EmployeeType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_EmployeeType_EmployeeTypeId",
+                table: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employee_EmployeeTypeId",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeTypeId",
+                table: "Employee");
+        }
+    }
+}
