10d719e685bc9ada337722806fe97bbbdee09752
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-08 11:21:24 +0700

    add comment general props

--- Project/API/Models/Common/GeneralProps.cs
+++ Project/API/Models/Common/GeneralProps.cs
@@ -2,9 +2,13 @@
 {
     public abstract class GeneralProps
     {
+        // Người tạo
         public string CreatedBy { get; set; }
+        // Ngày tạo
         public DateTime CreatedAt { get; set; }
+        // Người cập nhật
         public string UpdatedBy { get; set; }
+        // Ngày cập nhật
         public DateTime UpdatedAt { get; set; }
     }
 }
