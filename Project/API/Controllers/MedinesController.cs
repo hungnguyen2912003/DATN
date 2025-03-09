e52546399ccc0f168b3faaf1a06afbd3bd39a6a8
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-09 12:51:13 +0700

    RESTFUL Medicine

--- Project/API/Controllers/MedinesController.cs
+++ Project/API/Controllers/MedinesController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.Models.DTOs.Request.Medicine;
+using API.Models.DTOs.Response.Medicine;
+using API.Models.Entities;
+using API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Controllers
 {
@@ -7,5 +12,80 @@ namespace API.Controllers
     [ApiController]
     public class MedinesController : ControllerBase
     {
+        private readonly IMedicineRepository medicineRepository;
+
+        public MedinesController(IMedicineRepository medicineRepository)
+        {
+            this.medicineRepository = medicineRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMedicines()
+        {
+            var medicines = await medicineRepository.GetAllAsync();
+            return Ok(medicines);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetMedicineById([FromRoute]Guid id)
+        {
+            var medicine = await medicineRepository.GetByIdAsync(id);
+            if (medicine == null)
+            {
+                return NotFound();
+            }
+            return Ok(medicine);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMedicine([FromBody] CreateMedicineRequestDTO request)
+        {
+            //Map DTO <-> Domain: Create Medicine
+            var medicine = new Medicine
+            {
+                MedicineCode = request.MedicineCode,
+                Name = request.Name,
+                Description = request.Description,
+                Image = request.Image,
+                Price = request.Price,
+                InStock = request.InStock,
+                UnitQuantity = request.UnitQuantity,
+                UnitType = request.UnitType,
+                DosageQuantity = request.DosageQuantity,
+                DosageUnit = request.DosageUnit,
+                Manufacturer = request.Manufacturer,
+                ManufacturedDate = request.ManufacturedDate,
+                ActiveIngredient = request.ActiveIngredient,
+                ExpiryDate = request.ExpiryDate,
+                Status = request.Status
+            };
+
+            await medicineRepository.CreateAsync(medicine);
+
+            // Map Domain <-> DTO: Response Medicine
+
+            var response = new MedicineResponseDTO
+            {
+                Id = medicine.Id,
+                MedicineCode = medicine.MedicineCode,
+                Name = medicine.Name,
+                Description = medicine.Description,
+                Image = medicine.Image,
+                Price = medicine.Price,
+                InStock = medicine.InStock,
+                UnitQuantity = medicine.UnitQuantity,
+                UnitType = medicine.UnitType,
+                DosageQuantity = medicine.DosageQuantity,
+                DosageUnit = medicine.DosageUnit,
+                Manufacturer = medicine.Manufacturer,
+                ManufacturedDate = medicine.ManufacturedDate,
+                ActiveIngredient = medicine.ActiveIngredient,
+                ExpiryDate = medicine.ExpiryDate,
+                Status = medicine.Status
+            };
+
+            return Ok(response);
+        }
     }
 }
---
+++ Project/API/Models/DTOs/Response/Medicine/MedicineResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using API.Models.Enums.Medicine;
+
+namespace API.Models.DTOs.Response.Medicine
+{
+    public class MedicineResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string? MedicineCode { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public decimal Price { get; set; }
+        public int InStock { get; set; }
+        public int UnitQuantity { get; set; }
+        public UnitType UnitType { get; set; }
+        public decimal DosageQuantity { get; set; }
+        public DosageUnit DosageUnit { get; set; }
+        public string? Manufacturer { get; set; }
+        public DateTime ManufacturedDate { get; set; }
+        public string? ActiveIngredient { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
