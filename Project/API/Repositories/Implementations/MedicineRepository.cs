419d83e8b93131949cdb51b9c1e899d273c3f6ac
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-09 12:51:02 +0700

    Medicine Repository

---
+++ Project/API/Repositories/Implementations/MedicineRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Models.Common;
+using API.Models.Entities;
+
+namespace API.Repositories.Implementations
+{
+    public class MedicineRepository : BaseRepository<Medicine>
+    {
+        public MedicineRepository(HospitalDBContext context) : base(context)
+        {
+        }
+    }
+}
