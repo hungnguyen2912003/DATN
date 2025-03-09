52e09c74c5b955e6dc44d0d68a22278e65b95d7f
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-09 12:50:49 +0700

    organize Repository

---
+++ Project/API/Repositories/Implementations/BaseRepository.cs
@@ -0,0 +1,59 @@
+﻿using API.Models.Common;
+using API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories.Implementations
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly HospitalDBContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(HospitalDBContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T?> DeleteAsync(Guid id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null)
+            {
+                return null;
+            }
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if(entity == null)
+            {
+                return null;
+            }
+            return entity;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
